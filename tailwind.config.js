09a27c0b62302d61e36ed5d18529fde2b4714766
João Pster <joaopsterdev@gmail.com>
2022-07-01 20:01:31 -0300

    Adicionado os arquivos do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        backcolor: '#D8D8D8',
+        notifigreen: '#00D954',
+        oceanblue: '#5A5773',
+        bloodred: '#FF0000',
+      }
+    },
+  },
+  plugins: [],
+}
