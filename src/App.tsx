d105acfafca74f9a8709704eb944639ee7dd5e1b
João Pster <joaopsterdev@gmail.com>
2022-07-01 20:01:09 -0300

    Modificado o App e Adicionado o Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Header from './components/header';
+import LeftMenu from './components/leftmenu';
 import './App.css';
 
 function App() {
+  const [isLateralOpen, setIsLateralOpen] = useState(false);
+  const [deviceSize, changeDeviceSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeW = () => changeDeviceSize(window.innerWidth);
+    window.addEventListener("resize", resizeW);
+
+    if (deviceSize > 768) setIsLateralOpen(true);
+
+    return () => {
+      window.removeEventListener("resize", resizeW);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row flex-nowrap">
+      {isLateralOpen && (
+        <div className='flex grow-0 shrink-0 basis-20 min-h-screen'>
+          <LeftMenu />
+        </div>
+      )}
+      <div className="flex grow">
+        <div className="flex grow flex-col flex-nowrap">
+          <Header callback={setIsLateralOpen} open={isLateralOpen}/>
+          <div className="flex-initial p-5">
+          <p className="lg-font-text">
+            Indicadores
+          </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
